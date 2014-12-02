a7fe658323b8a7dd07d79aae26b7b8da764a084d
John Otander <johnotander@gmail.com>
2014-12-01 21:02:19 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var cssUrl = require('..');
+
+describe('css-url-regex', function() {
+
+  it('should find a css url with no quotes', function() {
+    assert.equal(cssUrl().test('url(foo.css)'), true);
+  });
+
+  it('should find a css url with quotes', function() {
+    assert.equal(cssUrl().test("url('foo.css')"), true);
+  });
+
+  it('should not find a css url if it is not there', function() {
+    assert.equal(cssUrl().test("('foo.css')"), false);
+  });
+});
